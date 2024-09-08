c6ab3c5cd902e8fef70f38a60ce473baba1e325a
Stanislav Varnavsky <phlx@ya.ru>
2024-09-09 01:08:00 +0300

    JWT v4 -> v5

--- go.mod
+++ go.mod
@@ -12,7 +12,7 @@ require (
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.11.2
-	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/stretchr/testify v1.8.2
 	github.com/tidwall/gjson v1.14.4
 	golang.org/x/crypto v0.6.0
--- jwt/jwt.go
+++ jwt/jwt.go
@@ -3,22 +3,22 @@ package jwt
 import (
 	"time"
 
-	jwtv4 "github.com/golang-jwt/jwt/v4"
+	jwtv5 "github.com/golang-jwt/jwt/v5"
 )
 
-func Check(secret string) func(token *jwtv4.Token) (interface{}, error) {
-	return func(_ *jwtv4.Token) (interface{}, error) {
+func Check(secret string) func(token *jwtv5.Token) (interface{}, error) {
+	return func(_ *jwtv5.Token) (interface{}, error) {
 		return []byte(secret), nil
 	}
 }
 
 func Make(issuer string, secret string) string {
-	claims := &jwtv4.RegisteredClaims{
-		ExpiresAt: jwtv4.NewNumericDate(time.Now().Add(365 * 24 * time.Hour)),
+	claims := &jwtv5.RegisteredClaims{
+		ExpiresAt: jwtv5.NewNumericDate(time.Now().Add(365 * 24 * time.Hour)),
 		Issuer:    issuer,
 	}
 
-	token := jwtv4.NewWithClaims(jwtv4.SigningMethodHS256, claims)
+	token := jwtv5.NewWithClaims(jwtv5.SigningMethodHS256, claims)
 	signedString, err := token.SignedString([]byte(secret))
 	if err != nil {
 		panic(err)
